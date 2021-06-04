d6b162354e3d8c34b11104009467818e82810b9c
kru4ara <kru4ara@gmail.com>
2021-06-04 16:09:21 +0300

    refactoring Dialogs component

--- app-bookface/src/components/Dialogs/Dialogs.jsx
+++ app-bookface/src/components/Dialogs/Dialogs.jsx
@@ -1,26 +1,40 @@
-import classes from './Dialogs.module.css'
+import classes from "./Dialogs.module.css";
+import { NavLink } from "react-router-dom";
+
+const DialogItem = (props) => {
+  const path = "/dialogs/" + props.id;
+  return (
+    <div className={classes.dialog}>
+      <NavLink to={path} activeClassName="activ">
+        {props.name}
+      </NavLink>
+    </div>
+  );
+};
+
+const Message = (props) => {
+  return <div className={classes.message}>{props.message}</div>;
+};
 
 const Dialogs = (props) => {
-    return (
-        <div className={classes.dialogs}>
-            <div className={classes.dialogsItems}>
-                <div className={classes.dialog}>Anatoli</div>
-                <div className={classes.dialog}>Alexandr</div>
-                <div className={classes.dialog}>Dmitry</div>
-                <div className={classes.dialog}>Michail</div>
-                <div className={classes.dialog}>Alena</div>
-                <div className={classes.dialog}>Lorisa</div>
-            </div>
-            <div className={classes.messages}>
-                <div className={classes.message}>hi</div>
-                <div className={classes.message}>hi</div>
-                <div className={classes.message}>hi</div>
-                <div className={classes.message}>hi</div>
-                <div className={classes.message}>hi</div>
-                <div className={classes.message}>hi</div>
-            </div>
-        </div>
-    );
+  return (
+    <div className={classes.dialogs}>
+      <div className={classes.dialogsItems}>
+        <DialogItem name="Anatoli" id="1" />
+        <DialogItem name="Alexandr" id="2" />
+        <DialogItem name="Dmitry" id="3" />
+        <DialogItem name="Michail" id="4" />
+        <DialogItem name="Alena" id="5" />
+        <DialogItem name="Lorisa" id="6" />
+      </div>
+      <div className={classes.messages}>
+        <Message message="Hello" />
+        <Message message="How are you?" />
+        <Message message="What are you doing?" />
+        <Message message="Can meet?" />
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
