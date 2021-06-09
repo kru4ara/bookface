1732bf2c9a8adeb54925d5d26fc3c89cba23e313
kru4ara <kru4ara@gmail.com>
2021-06-09 16:57:15 +0300

    install redux

--- app-bookface/src/index.js
+++ app-bookface/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './App';
-import store from './redux/state';
+import store from './redux/store';
 
 const rerenderEntireTree = (state) => {
   ReactDOM.render(
